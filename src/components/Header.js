63c5270091c65d6e619a61e6f00c43c91680b7c9
orangeola <orangeqmm@gmail.com>
2023-02-19 20:57:37 +1030

    Set up starter files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className="Header">
+      <p>Link to Homepage</p>
+      <p>Link to Shop</p>
+      <p>Cart</p>
+    </div>
+  );
+}
+
+export default Header;
